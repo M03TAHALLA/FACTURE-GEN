0c3680d5a5fb4e6ed28e38b73e4b116adece0769
ABDERRAHMANE AITELMOUDDENE <abderrahmaneaitel@gmail.com>
2022-12-17 22:58:39 +0100

    split the nav-foot.php

--- src/Commun/nav.php
+++ src/Commun/nav.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
 
 
 
@@ -62,35 +50,3 @@
 <!-- NAVBAR -->
 
 
-
-<!-- FOOTER -->
-
-<footer class="text-center text-white fixed-bottom" style="background-color: #f1f1f1;">
-
-
-  <!-- Copyright -->
-  <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2023 Copyright:
-    <a class="text-dark" href="https://IN-VOICER.com/">IN-voicer.com</a>
-  </div>
-  <!-- Copyright -->
-
-</footer>
-
-
-<!-- FOOTER -->
-
-
-
-
-
-
-
-
-
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-
-  </body>
-</html>
