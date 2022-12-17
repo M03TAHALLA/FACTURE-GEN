4c988938c587e5cb3eba21e850a9f12271e94ea2
ABDERRAHMANE AITELMOUDDENE <abderrahmaneaitel@gmail.com>
2022-12-17 21:54:02 +0100

    modified the navbar and added a footer

--- src/nav-foot.php
+++ src/nav-foot.php
@@ -1,29 +1,96 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</head>
-<body>
-<!-- Navbar -->
-<nav class="navbar navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">
-    <img src="..\media\INv.webp" width="60" height="60"  class="rounded d-inline-block " alt="">
-    IN-VOICER
-  </a>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+
+
+
+  <!-- NAVBAR -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 justify-content-center">
+
+  <a href=""><img src="..\media\INv.webp" width="60" height="60"  class="rounded  bg-dark" alt=""></a>
+    <a class="navbar-brand ps-1" href="#">    IN-VOICER</a>
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Devis</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link active" href="#">Bons de commande</a>
+        </li>
+
+        <li class="nav-item ">
+          <a class="nav-link active" href="#">Creer DV</a>
+        </li>
+
+        <li class="nav-item ">
+          <a class="nav-link active" href="#">Creer BDC</a>
+        </li>
+
+        
+      </ul>
+      
+      <div class="nav-item dropdown justify-content-end pe-5 me-2">
+          <a class="nav-link link-dark dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Facturation</a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">ABOUT US</a></li>
+            <li><a class="dropdown-item" href="#">CONTACT US</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">LOGOUT</a></li>
+          </ul>
+      </div>
+    </div>
+
 </nav>
-<!-- Navbar -->
+<!-- NAVBAR -->
 
-</body>
+
+
+<!-- FOOTER -->
+
+<footer class="text-center text-white fixed-bottom" style="background-color: #f1f1f1;">
+
+
+  <!-- Copyright -->
+  <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Copyright:
+    <a class="text-dark" href="https://IN-VOICER.com/">IN-voicer.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+
+
+<!-- FOOTER -->
+
+
+
+
+
+
+
+
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+  </body>
 </html>
-<!-- 
-src="../media/reception-3.svg"
-src=""
-
-src="../media/arrow-down-square-fill.svg" -->
