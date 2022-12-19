470b045e9c03def8c8176d09956aef3149f70bfb
MOHAMMED TAHALLA <mohamdwiners@gmail.com>
2022-12-19 20:38:19 +0100

    Lier Entre Les Pages

--- src/Include/nav.php
+++ src/Include/nav.php
@@ -9,7 +9,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 justify-content-center">
 
   <a href=""><img src="..\..\media\INv.webp" width="60" height="60"  class="rounded  bg-dark" alt=""></a>
-    <a class="navbar-brand ps-1" href="#">    IN-VOICER</a>
+    <a class="navbar-brand ps-1" href="../Commun/home.php">    IN-VOICER</a>
     
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -20,18 +20,18 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Devis</a>
+          <a class="nav-link active" aria-current="page" href="../AffichageListes/Show_Devise_Liste.php">Devis</a>
         </li>
         <li class="nav-item ">
-          <a class="nav-link active" href="#">Bons de commande</a>
-        </li>
-
-        <li class="nav-item ">
-          <a class="nav-link active" href="#">Creer DV</a>
+          <a class="nav-link active" href="../AffichageListes/Show_Commandes_Liste.php">Bons de commande</a>
         </li>
 
         <li class="nav-item ">
-          <a class="nav-link active" href="#">Creer BDC</a>
+          <a class="nav-link active" href="../Facturation/CreateDevis.php">Creer DV</a>
+        </li>
+
+        <li class="nav-item ">
+          <a class="nav-link active" href="../Facturation/CreateBon_Commande.php">Creer BDC</a>
         </li>
 
         
@@ -40,8 +40,8 @@
       <div class="nav-item dropdown justify-content-end pe-5 me-2">
           <a class="nav-link link-dark dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Facturation</a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">ABOUT US</a></li>
-            <li><a class="dropdown-item" href="#">CONTACT US</a></li>
+            <li><a class="dropdown-item" href="../Commun/AboutUs.php">ABOUT US</a></li>
+            <li><a class="dropdown-item" href="../Commun/contactUs.php">CONTACT US</a></li>
             <li><hr class="dropdown-divider"></li>
             
               <?php
