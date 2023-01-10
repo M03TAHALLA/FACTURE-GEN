c44e2ee5d462793a37eeb0534c0e0a6dd04cc97a
ABDERRAHMANE AITELMOUDDENE <abderrahmaneaitel@gmail.com>
2023-01-10 01:36:05 +0100

    changing DB info
    Updating the old Database connection informations

--- db/conn.php
+++ db/conn.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "sql7.freesqldatabase.com";
-$username = "sql7586075";
-$password = "lit9GXL9wY";
+$username = "sql7589420";
+$password = "4fYqwFelZq";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $username);
